cb52884b00b845f7e0c8d9a861f6d7e9cec807f2
AlexKar0706 <53472100+AlexKar0706@users.noreply.github.com>
2021-11-20 22:54:14 +0200

    Update main.c

--- LabX_LowPowerMode/main.c
+++ LabX_LowPowerMode/main.c
@@ -49,11 +49,11 @@ void PortF_Init(void){ volatile unsigned long delay;
 */
 int main (void) {
   UART_Init();    			// initialize UART for printing
-	PortF_Init();   			// initialize Port F for Green LED
+	PortF_Init();   			// initialize Port F for Green and Red LED
 	Temp_Sensor_Init();		// initialize temperature sensort
 	printf("\nStarting active mode programm\n");
 	while (1) {
-		Delay(5000);								// Simulate 5 seconds delay
+		Delay(2000);								// Simulate 5 seconds delay
 		Print_Temp();								// Print temperature
 	}
 }
@@ -63,9 +63,9 @@ int main (void) {
 */
 int main2 (void) {
   UART_Init();    					// initialize UART for printing
-	PortF_Init();   					// initialize Port F for Green LED
+	PortF_Init();   					// initialize Port F for Green and Red LED
 	Temp_Sensor_Init();  			// initialize temperature sensort
-	SysTick_Init(0x1000000);  // initialize  NVIC interrupts for sleep mode
+	SysTick_Init(0);  // initialize  NVIC interrupts for sleep mode
 	printf("\nStarting sleep mode programm\n");
 	WaitForInterrupt();       // Put microcontroller in sleep mode
 	return 0;
@@ -76,11 +76,11 @@ int main2 (void) {
 */
 int main3 (void) {
   UART_Init();            		// initialize UART for printing
-	PortF_Init();           		// initialize Port F for Green LED
+	PortF_Init();           		// initialize Port F for Green and Red LED
 	Hibernate_Mode_Init();  		// initialize hibernation mode
 	Temp_Sensor_Init();     		// initialize temperature sensort
 	
-	if (!(HIB_RIS_R&0x01)) printf("\nStarting hibernation mode programm\n");
+	if (!(HIB_RIS_R&0x01)) printf("\nStarting hibernation mode programm\n");  // Check for Hibernation interrupt bit
 	else printf("\nWaking up from hibernation\n");
 	
 	Delay(25);                  // Give 25ms delay for microcontroller to stabilize system after wakining up
@@ -89,7 +89,7 @@ int main3 (void) {
 	GPIO_PORTF_DATA_R |= 0x08; // Turn on LED to acknowledge, that microcontroller is currently active
 	Delay(5000);								// Simulate 5 seconds delay
 	
-	GPIO_PORTF_DATA_R &= ~0x08; // Turn off LED after printing temperature
+	GPIO_PORTF_DATA_R &= ~0x08; // Turn off LED after 5 seconds delay
 	Activate_Hibernate();				// Go to hibernation
 	while (1) {	
 	}
